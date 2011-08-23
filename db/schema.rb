59937d3b8d08faca329292d9e201fd46b11c7a92
Jared Szechy <jared.szechy@gmail.com>
2011-08-23 00:00:18 -0400

    -Add admin role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110823024518) do
+ActiveRecord::Schema.define(:version => 20110823034814) do
 
   create_table "cores", :force => true do |t|
     t.string   "name"
@@ -48,8 +48,8 @@ ActiveRecord::Schema.define(:version => 20110823024518) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                                 :default => "",    :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20110823024518) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
+    t.boolean  "admin",                                 :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
